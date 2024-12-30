3eae7f8601c43ed32fc48067b71c0d23d8a8c06d
夏愚[XYU] <zhao.qianli@gmail.com>
2024-12-30 15:53:58 +0800

    update

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -2,6 +2,13 @@
 ;; (any change needs a theme re-load to take effect).
 ;;自动换行
 
+;; theme
+(require 'ef-themes)
+(load-theme 'ef-cyprus t)
+
+;; 设置字体
+(set-face-attribute 'default nil :height 160)
+
 (provide 'init-local)
 
 ;;
